23173176686d1deb376c82a72134224eb37ec4b4
Andre <gorduranogueirapissuto@gmail.com>
2025-06-15 01:02:20 -0300

    criacao e manipulacoes basicas nocadastroComponent

--- ProjetoFuncionalidadesUsuario/src/app/app.component.ts
+++ ProjetoFuncionalidadesUsuario/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, CadastroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
